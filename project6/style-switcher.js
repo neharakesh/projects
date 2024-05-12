b5420fd36f725d49e7cfb598c874df2cfd200f51
Neha Rajpoot <132067615+neharakesh@users.noreply.github.com>
2024-05-12 06:59:43 +0530

    Add files via upload

---
+++ project6/style-switcher.js
@@ -0,0 +1,26 @@
+/*=======toggel style switcher========*/
+console.log("s");
+const styleSwitcherToggel = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggel.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+//hide style switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector("style-switcher").classList.remove("open");
+    }
+})
+/*================theme colors=====================*/
+const alternateStyles= document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach(style) =>{
+    if(color== style.getAttribute("title"))
+    {
+        style.removeAttribute("disabled");
+    }
+    else{
+        style.setAttribute("disabled","true");
+    }
+}}
+
